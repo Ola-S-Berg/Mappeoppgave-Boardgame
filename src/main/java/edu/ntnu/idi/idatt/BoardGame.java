04341f1f81d7ff1a1af66b7379725f418761eb16
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-02-15 17:10:15 +0100

    Updated class BoardGame.java
    Created some methods to be filled in as other classes are created and implemented.

--- src/main/java/edu/ntnu/idi/idatt/BoardGame.java
+++ src/main/java/edu/ntnu/idi/idatt/BoardGame.java
@@ -1,5 +1,35 @@
 package edu.ntnu.idi.idatt;
+import java.util.List;
 
+/**
+ * Class responsible for the game. Sets up the board, sets up dice, registers players
+ * and plays the game by iterating over the players, letting them roll dice and move.
+ */
 public class BoardGame {
+  private Board board;
+  private Player currentPlayer;
+  private List<Player> players;
+  private Dice dice;
 
+  //Add constructor here.
+
+  //Fill in methods as
+  public void addPlayer(String name) {
+  }
+
+  public void createBoard() {
+
+  }
+
+  public void createDice() {
+
+  }
+
+  public void play() {
+
+  }
+
+  public Player getWinner() {
+    return null;
+  }
 }
