ccf650929cba3b5f01495427416b6e5a53d51b52
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-02-15 10:07:17 +0100

    Created test-class DieTest
    Created and implemented a test class for class Die.

--- src/main/java/edu/ntnu/idi/idatt/Dice.java
+++ src/main/java/edu/ntnu/idi/idatt/Dice.java
@@ -1,7 +1,7 @@
 package edu.ntnu.idi.idatt;
 
 /**
- * Class representing a collection of die. 
+ * Class representing a collection of die.
  */
 public class Dice {
 
---
+++ src/test/java/edu/ntnu/idi/idatt/DieTest.java
@@ -0,0 +1,22 @@
+package edu.ntnu.idi.idatt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test-class for class Die. Tests rolling a die and asserts true if the value is between 1 and 6.
+ */
+class DieTest {
+
+  @Test
+  void testRollDie() {
+    Die die = new Die();
+    int result = die.roll();
+    assertTrue(result >= 1 && result <= 6);
+  }
+
+  @Test
+  void getValue() {
+  }
+}
