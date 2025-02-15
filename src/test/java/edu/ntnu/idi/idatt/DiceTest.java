010cb977dd299ccfd0d429755967e31659afe292
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-02-15 10:26:50 +0100

    Created DiceTest.java
    Created and implemented test-class for class Dice.

---
+++ src/test/java/edu/ntnu/idi/idatt/DiceTest.java
@@ -0,0 +1,24 @@
+package edu.ntnu.idi.idatt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test-class for class Dice. Sets up two dice and rolls them. Asserts true if value is between 2 and 12.
+ */
+class DiceTest {
+  private Dice dice;
+
+  @BeforeEach
+  void setUp() {
+    dice = new Dice(2);
+  }
+
+  @Test
+  void testRollDice() {
+    int result = dice.roll();
+    assertTrue(result >= 2 && result <= 12);
+  }
+}
