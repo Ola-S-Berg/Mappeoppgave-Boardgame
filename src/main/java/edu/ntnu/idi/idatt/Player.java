c120cdcbf7fd1d189135ca68679d0fc92f58c9bc
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-02-21 19:24:19 +0100

    Updated player.java
    Created constructor.
    Created method placeOnTile (WIP).
    Created method move (WIP).

--- src/main/java/edu/ntnu/idi/idatt/Player.java
+++ src/main/java/edu/ntnu/idi/idatt/Player.java
@@ -4,5 +4,19 @@ package edu.ntnu.idi.idatt;
  * Class representing a player in the game
  */
 public class Player {
+  private String name;
+  private Tile currentTile;
 
+  public Player(String name, Tile currentTile) {
+    this.name = name;
+    this.currentTile = currentTile;
+  }
+
+  public void placeOnTile (Tile tile) {
+
+  }
+
+  public void move (int steps) {
+    
+  }
 }
