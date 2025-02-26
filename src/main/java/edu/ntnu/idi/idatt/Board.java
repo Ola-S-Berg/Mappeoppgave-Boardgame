aef4e81a375d9156e0d33f59f038f97be1002fbe
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-02-26 23:38:30 +0100

    Updated board.java
    Added javadoc to addTile and getTile.

--- src/main/java/edu/ntnu/idi/idatt/Board.java
+++ src/main/java/edu/ntnu/idi/idatt/Board.java
@@ -6,18 +6,25 @@ import java.util.HashMap;
  * Class representing the game board. Consists of a collection of tiles.
  * Also keeps a complete overview of all tiles.
  */
-
 public class Board {
   private Map<Integer, Tile> tiles = new HashMap<>();
 
+  /**
+   * Adds a tile to the board.
+   * @param tile The tile to add.
+   */
   public void addTile(Tile tile) {
     tiles.put(tile.getTileId(), tile);
   }
 
+  /**
+   * Accessor that gets the ID (number) of the tile.
+   * @param tileId The tile ID represented by an integer.
+   * @return The ID.
+   */
   public Tile getTile(int tileId) {
     return tiles.get(tileId);
   }
-
 }
 
 
