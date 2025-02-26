fdb107a12daa617e45b056ac1b64a5a93b6e0f6b
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-02-27 00:11:31 +0100

    Updated LadderAction and TileAction
    Added javadoc to TileAction.
    Added description to constructor for LadderAction

--- src/main/java/edu/ntnu/idi/idatt/LadderAction.java
+++ src/main/java/edu/ntnu/idi/idatt/LadderAction.java
@@ -5,8 +5,10 @@ package edu.ntnu.idi.idatt;
  */
 public class LadderAction {
   private int destinationTileId;
-
-  public LadderAction(int destinationTileId) {
+  private String description;
+  
+  public LadderAction(int destinationTileId, String description) {
     this.destinationTileId = destinationTileId;
+    this.description = description;
   }
 }
