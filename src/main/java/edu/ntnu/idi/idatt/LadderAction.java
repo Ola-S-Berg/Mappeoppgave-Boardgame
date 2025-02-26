82adb6ba9f1deadcade0795755c699e73921252d
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-02-27 00:00:56 +0100

    Updated LadderAction.java
    Defined destinationTileId and it's constructor.

--- src/main/java/edu/ntnu/idi/idatt/LadderAction.java
+++ src/main/java/edu/ntnu/idi/idatt/LadderAction.java
@@ -1,5 +1,12 @@
 package edu.ntnu.idi.idatt;
 
+/**
+ *
+ */
 public class LadderAction {
+  private int destinationTileId;
 
+  public LadderAction(int destinationTileId) {
+    this.destinationTileId = destinationTileId;
+  }
 }
