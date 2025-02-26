dcaba6d0b2c459a0dd26552b73cb84d03699a7af
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-02-26 23:31:40 +0100

    Updated Tile.java and Board.java
    Created accessor methods getTileId and getNextTile in class Tile.
    Created addTile method in class board.

--- src/main/java/edu/ntnu/idi/idatt/Board.java
+++ src/main/java/edu/ntnu/idi/idatt/Board.java
@@ -5,14 +5,13 @@ import java.util.HashMap;
 /**
  * Class representing the game board. Consists of a collection of tiles.
  * Also keeps a complete overview of all tiles.
- * Note: Wait with implementing until the creation and implementation of class Tile and TileAction.
  */
 
 public class Board {
   private Map<Integer, Tile> tiles = new HashMap<>();
 
-  public void addTile() {
-
+  public void addTile(Tile tile) {
+    tiles.put(tile.getTileId(), tile);
   }
 
   public Tile getTile(int tileId) {
