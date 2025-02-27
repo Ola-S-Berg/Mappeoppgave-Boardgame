90df87cbcdf855c0a10c497f4cf9f1c3b7397227
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-02-27 01:12:53 +0100

    Updated BoardGame.java
    Created methods for:
    Adding a players to the game.
    Creating the board.
    Creating a number of dice.

--- src/main/java/edu/ntnu/idi/idatt/BoardGame.java
+++ src/main/java/edu/ntnu/idi/idatt/BoardGame.java
@@ -1,4 +1,25 @@
 package edu.ntnu.idi.idatt;
+import java.util.List;
+import java.util.ArrayList;
 
+/**
+ * The class that holds
+ */
 public class BoardGame {
+  private Board board;
+  private Player currentPlayer;
+  private List<Player> players = new ArrayList<>();
+  private Dice dice;
+
+  public void addPlayer (Player player) {
+    players.add(player);
+  }
+
+  public void createBoard() {
+    board = new Board();
+  }
+
+  public void createDice() {
+    dice = new Dice(2);
+  }
 }
