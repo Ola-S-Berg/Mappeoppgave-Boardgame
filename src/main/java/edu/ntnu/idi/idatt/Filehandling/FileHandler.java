77d0a40660bc800b3cf659861a553000b2160dda
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-03-24 16:17:35 +0100

    Created fileHandler.java
    Created interface for writing and reading from files.

---
+++ src/main/java/edu/ntnu/idi/idatt/Filehandling/FileHandler.java
@@ -0,0 +1,8 @@
+package edu.ntnu.idi.idatt.Filehandling;
+
+/**
+ * Interface for file handling. Defines methods for reading and writing objects to files.
+ */
+public interface FileHandler {
+  void saveToFile(String filename);
+}
