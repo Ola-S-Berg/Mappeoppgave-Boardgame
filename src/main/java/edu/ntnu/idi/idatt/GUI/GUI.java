994b2bf4eb3f87117c12f6c8b589059ff879205a
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-03-27 19:19:04 +0100

    Created initial GUI
    Created initial GUI for LadderGame

--- src/main/java/edu/ntnu/idi/idatt/GUI/GUI.java
+++ src/main/java/edu/ntnu/idi/idatt/GUI/GUI.java
@@ -1,15 +1,28 @@
 package edu.ntnu.idi.idatt.GUI;
 
-import javafx.scene.control.*;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
 
   @Override
   public void start(Stage primaryStage) {
+
     Button playButton = new Button("Play");
+
+    VBox layout = new VBox(10);
+    layout.getChildren().add(playButton);
+
+    Scene scene = new Scene(layout, 300, 200);
+
+    primaryStage.setTitle("Ladder game");
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
+
   public static void main(String[] args) {
     launch(args);
   }
