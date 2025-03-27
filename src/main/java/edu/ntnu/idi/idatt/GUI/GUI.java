3d848f020638672ea0d31203c74764ebfe306c9f
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-03-27 12:52:22 +0100

    Created GUI.java
    Created class GUI.java a basic application to get javafx running

---
+++ src/main/java/edu/ntnu/idi/idatt/GUI/GUI.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idi.idatt.GUI;
+
+import javafx.scene.control.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+  @Override
+  public void start(Stage primaryStage) {
+    Button playButton = new Button("Play");
+  }
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
