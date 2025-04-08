f64fdc712c5a92e4986439c723a146605701f127
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-04-08 13:55:11 +0200

    Added javadoc

--- src/main/java/edu/ntnu/idi/idatt/Actions/BackToStartAction.java
+++ src/main/java/edu/ntnu/idi/idatt/Actions/BackToStartAction.java
@@ -3,9 +3,14 @@ package edu.ntnu.idi.idatt.Actions;
 import edu.ntnu.idi.idatt.GameLogic.Player;
 
 /**
- * 
+ * Class representing action when landing on a move back to start tile.
  */
-public class BackToStartAction implements TileAction{
+public class BackToStartAction implements TileAction {
+
+  /**
+   * Performs the action of moving the player back to start.
+   * @param player The player that lands on the tile with this action.
+   */
   public void perform(Player player) {
   }
 }
--- src/main/java/edu/ntnu/idi/idatt/Actions/WaitAction.java
+++ src/main/java/edu/ntnu/idi/idatt/Actions/WaitAction.java
@@ -3,13 +3,13 @@ package edu.ntnu.idi.idatt.Actions;
 import edu.ntnu.idi.idatt.GameLogic.Player;
 
 /**
- * Placeholder class for other actions.
+ * Class representing action when landing on a wait tile.
  */
 public class WaitAction implements TileAction {
 
   /**
-   * Does an action.
-   * @param player Some player action.
+   * Performs the action of making the player wait a turn before rolling again.
+   * @param player The player that lands on the tile with this action.
    */
   public void perform(Player player) {
   }
