8a55f363780b30428c133e4429a64f0993a2f7d1
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-04-08 13:52:35 +0200

    Created BackToStartAction

---
+++ src/main/java/edu/ntnu/idi/idatt/Actions/BackToStartAction.java
@@ -0,0 +1,11 @@
+package edu.ntnu.idi.idatt.Actions;
+
+import edu.ntnu.idi.idatt.GameLogic.Player;
+
+/**
+ * 
+ */
+public class BackToStartAction implements TileAction{
+  public void perform(Player player) {
+  }
+}
