4bf232c4befea36ff1609e0678695144fb00b3e8
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-04-08 15:26:25 +0200

    Updated TileAction.java
    Changed description to direction as it makes more sense.
    Added methods to get destination and direction of ladders.

--- src/main/java/edu/ntnu/idi/idatt/Actions/BackToStartAction.java
+++ src/main/java/edu/ntnu/idi/idatt/Actions/BackToStartAction.java
@@ -12,6 +12,7 @@ public class BackToStartAction implements TileAction {
    * Performs the action of moving the player back to start.
    * @param player The player that lands on the tile with this action.
    */
+  @Override
   public void perform(Player player) {
     System.out.println(player.getName() + " moved back to start");
     Tile startTile = player.getGame().getBoard().getTile(1);
