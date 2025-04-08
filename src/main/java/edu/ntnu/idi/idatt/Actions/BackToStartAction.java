92370865627aab6faedf03790ddee3135bc16c4b
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-04-08 14:02:09 +0200

    Implemented logic for placing a player back on the start tile

--- src/main/java/edu/ntnu/idi/idatt/Actions/BackToStartAction.java
+++ src/main/java/edu/ntnu/idi/idatt/Actions/BackToStartAction.java
@@ -1,6 +1,7 @@
 package edu.ntnu.idi.idatt.Actions;
 
 import edu.ntnu.idi.idatt.GameLogic.Player;
+import edu.ntnu.idi.idatt.GameLogic.Tile;
 
 /**
  * Class representing action when landing on a move back to start tile.
@@ -12,5 +13,8 @@ public class BackToStartAction implements TileAction {
    * @param player The player that lands on the tile with this action.
    */
   public void perform(Player player) {
+    System.out.println(player.getName() + " moved back to start");
+    Tile startTile = player.getGame().getBoard().getTile(1);
+    player.placeOnTile(startTile);
   }
 }
