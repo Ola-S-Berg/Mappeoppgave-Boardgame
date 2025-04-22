01655f0cff97ee324b0d43d6a47e6993f1afaafe
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-04-23 00:23:13 +0200

    Added box for choosing a ladder game variation

--- src/main/java/edu/ntnu/idi/idatt/GUI/GameSelectionView.java
+++ src/main/java/edu/ntnu/idi/idatt/GUI/GameSelectionView.java
@@ -1,10 +1,16 @@
 package edu.ntnu.idi.idatt.GUI;
 
+import edu.ntnu.idi.idatt.Filehandling.BoardGameFactory;
+
+import edu.ntnu.idi.idatt.GameLogic.BoardGame;
+import java.util.List;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * View class for the game selection view.
@@ -34,17 +40,57 @@ public class GameSelectionView {
     titleLabel.setStyle("-fx-font-size: 24px");
     layout.getChildren().add(titleLabel);
 
-    String[] games = {"Ladder Game"};
+    String[] games = {"Ladder Game", "WIP", "WIP"};
 
     for (String game : games) {
       Button gameButton = new Button(game);
       gameButton.setOnAction(event -> {
-        application.showPlayerCountView(game);
+        if (game.equals("Ladder Game")) {
+          showLadderVariationsPopup();
+        } else {
+          application.showPlayerCountView(game);
+        }
       });
       layout.getChildren().add(gameButton);
     }
 
     scene = new Scene(layout, 800, 800);
+  }
+
+  /**
+   * Shows a popup for selecting ladder game variations.
+   */
+  private void showLadderVariationsPopup() {
+    Stage popup = new Stage();
+    popup.initModality(Modality.APPLICATION_MODAL);
+    popup.setTitle("Select Ladder Game Version");
+
+    VBox popupLayout = new VBox(15);
+    popupLayout.setAlignment(Pos.CENTER);
+
+    Label loadLabel = new Label("Load Saved Game");
+    loadLabel.setStyle("-fx-font-size: 24px");
+    popupLayout.getChildren().add(loadLabel);
+
+    Label label = new Label("Choose a variation:");
+    label.setStyle("-fx-font-size: 18px");
+    popupLayout.getChildren().add(label);
+
+    List<String> variations = BoardGameFactory.getAvailableBoardGames();
+
+    for (String variation : variations) {
+      Button variationButton = new Button(variation);
+      variationButton.setOnAction(event -> {
+        popup.close();
+        BoardGame selectedGame = BoardGameFactory.createBoardGame(variation);
+        application.showPlayerCountView(variation);
+      });
+      popupLayout.getChildren().add(variationButton);
+    }
+
+    Scene popupScene = new Scene(popupLayout, 500, 500);
+    popup.setScene(popupScene);
+    popup.showAndWait();
   }
 
   /**
