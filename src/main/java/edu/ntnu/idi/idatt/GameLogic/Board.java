9ff5bb9033a07e943d97b7823c3cdeead4a40a41
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-04-24 00:23:04 +0200

    Changed some ladderactions for testing

--- src/main/java/edu/ntnu/idi/idatt/GameLogic/Board.java
+++ src/main/java/edu/ntnu/idi/idatt/GameLogic/Board.java
@@ -57,19 +57,19 @@ public class Board {
   }
 
   public static void setupTileActionsLadderGame2(Board board) {
-    board.getTile(5).setAction(new LadderAction(17, "up"));
-    board.getTile(12).setAction(new LadderAction(49, "up"));
-    board.getTile(21).setAction(new LadderAction(41, "up"));
-    board.getTile(43).setAction(new LadderAction(61, "up"));
-    board.getTile(55).setAction(new LadderAction(87, "up"));
-    board.getTile(65).setAction(new LadderAction(84, "up"));
+    board.getTile(6).setAction(new LadderAction(17, "up"));
+    board.getTile(13).setAction(new LadderAction(49, "up"));
+    board.getTile(22).setAction(new LadderAction(41, "up"));
+    board.getTile(44).setAction(new LadderAction(61, "up"));
+    board.getTile(56).setAction(new LadderAction(87, "up"));
+    board.getTile(66).setAction(new LadderAction(84, "up"));
 
-    board.getTile(25).setAction(new LadderAction(7, "down"));
-    board.getTile(38).setAction(new LadderAction(1, "down"));
-    board.getTile(48).setAction(new LadderAction(13, "down"));
-    board.getTile(70).setAction(new LadderAction(30, "down"));
-    board.getTile(79).setAction(new LadderAction(27, "down"));
-    board.getTile(89).setAction(new LadderAction(53, "down"));
+    board.getTile(26).setAction(new LadderAction(7, "down"));
+    board.getTile(39).setAction(new LadderAction(1, "down"));
+    board.getTile(49).setAction(new LadderAction(13, "down"));
+    board.getTile(71).setAction(new LadderAction(30, "down"));
+    board.getTile(77).setAction(new LadderAction(27, "down"));
+    board.getTile(88).setAction(new LadderAction(53, "down"));
 
     board.getTile(37).setAction(new WaitAction());
     board.getTile(54).setAction(new WaitAction());
