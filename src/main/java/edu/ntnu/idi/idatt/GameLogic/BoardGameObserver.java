de065fb121b958eefd0957b295d8f72da1cf8739
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-04-23 22:01:02 +0200

    Created BoardGameObserver
    Observes changes in the gamestate

---
+++ src/main/java/edu/ntnu/idi/idatt/GameLogic/BoardGameObserver.java
@@ -0,0 +1,32 @@
+package edu.ntnu.idi.idatt.GameLogic;
+
+public interface BoardGameObserver {
+
+  /**
+   * Called when a player moves to a new tile.
+   * @param player The player that moved.
+   * @param fromTileId The ID of the tile the player moved from.
+   * @param toTileId The ID of the tile the player moved to.
+   * @param diceValue The value rolled on the dice.
+   */
+  void onPlayerMove(Player player, int fromTileId, int toTileId, int diceValue);
+
+  /**
+   * Called when a player wins the game.
+   * @param player The winning player.
+   */
+  void onGameWon(Player player);
+
+  /**
+   * Called when a player must skip their turn.
+   * @param player The player that must skip their turn.
+   */
+  void onPlayerSkipTurn(Player player);
+
+  /**
+   * Called when the current player changes.
+   * @param player The new current player.
+   */
+  void onCurrentPlayerChanged(Player player);
+
+}
