e81e6de6804096da9f921cd3e9e8a3395ae05bce
Markus Øyen Lund <markuol@stud.ntnu.no>
2025-05-20 16:23:37 +0200

    Javadocced BoardGameTest

--- src/test/java/edu/ntnu/idi/idatt/model/gamelogic/BoardGameTest.java
+++ src/test/java/edu/ntnu/idi/idatt/model/gamelogic/BoardGameTest.java
@@ -9,11 +9,13 @@ class BoardGameTest {
   
   private BoardGame boardGame;
 
+  /* Set up the board game before each test */
   @BeforeEach
   void setUp() {
     boardGame = new BoardGame();
   }
 
+  /* Test that the addPlayer method works by adding a player and checking if the player is added successfully */
   @Test
   void addPlayer_Positive() {
    
@@ -24,12 +26,14 @@ class BoardGameTest {
     assertEquals(player, boardGame.getPlayers().get(0), "Player should be added successfully.");
   }
 
+  /* Test that the addPlayer method throws an exception when adding a null player */
   @Test
   void addPlayer_Negative_NullPlayer() {
     
     assertThrows(NullPointerException.class, () -> boardGame.addPlayer(null), "Should not allow adding a null player.");
   }
 
+  /* Test that the createLadderGameBoard method works by getting a board and checking if the tile 90 exists */
   @Test
   void createLadderGameBoard_Positive() {
    
@@ -39,19 +43,22 @@ class BoardGameTest {
     assertEquals(90, boardGame.getBoard().getTile(90).getTileId(), "Tile 90 should exist.");
   }
 
+  /* Test that the createLadderGameBoard method throws an exception when accessing the board before initialization */
   @Test
   void createLadderGameBoard_Negative_BeforeInitialization() {
     
     assertThrows(NullPointerException.class, () -> boardGame.getBoard().getTile(1), "Should not allow accessing board before initialization.");
   }
 
+  /* Test that the createDice method works */
   @Test
   void createDice_Positive() {
     
     boardGame.createDice();
     assertNotNull(boardGame.getDice(), "Dice should be initialized.");
-}
+  }
 
+  /* Test that the playGame method works */
   @Test
   void playGame_Positive() {
     
